5f0175b31dfa1cf3b36ea1beb8fd31547116a451
Eric Hu and Jordi Noguera <pair+ehu+jordi@pivotallabs.com>
2014-06-13 09:19:47 -0400

    Remove setup script no longer being used

--- scripts/setup_buildpacks_environment.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/cloudfoundry/cf-buildpack-ruby ../buildpacks/cf-buildpack-ruby
-git clone https://github.com/pivotal-cf-experimental/cf-buildpack-go ../buildpacks/cf-buildpack-go
-git clone https://github.com/pivotal-cf-experimental/cf-buildpack-nodejs ../buildpacks/cf-buildpack-nodejs
