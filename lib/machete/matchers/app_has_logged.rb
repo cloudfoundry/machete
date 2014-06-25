efcd42e5250757b0b24aa94fc8efc28bc8f6e125
Tim Johnson <pair+buddhistpirate@pivotallabs.com>
2014-06-25 11:43:17 -0400

    Add proper failure messages for has_logged
    [#73723814]

--- lib/machete/matchers/app_has_logged.rb
+++ lib/machete/matchers/app_has_logged.rb
@@ -4,5 +4,18 @@ RSpec::Matchers.define :have_logged do |expected_entry|
   match do |app|
     app_log = Machete::CF::AppLog.new(app)
     app_log.contents.include? expected_entry
+
+  end
+
+  failure_message do |app|
+    app_log = Machete::CF::AppLog.new(app)
+    "\nApp log did not include #{expected_entry} \n\n" +
+      app_log.contents
+  end
+
+  failure_message_when_negated do |app|
+    app_log = Machete::CF::AppLog.new(app)
+    "\nApp log did include #{expected_entry} \n\n" +
+      app_log.contents
   end
 end
