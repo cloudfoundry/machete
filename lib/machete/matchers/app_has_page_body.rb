b0455005ca7af318c2613c93bc92f158f54c921a
Kyle McConnaughay and Tim Johnson <pair+kyle.mcconnaughay+buddhistpirate@pivotallabs.com>
2014-07-07 11:03:08 -0400

    Include page body on page body matcher failures
    [#72637582]

--- lib/machete/matchers/app_has_page_body.rb
+++ lib/machete/matchers/app_has_page_body.rb
@@ -1,8 +1,20 @@
 require 'rspec/matchers'
 
 RSpec::Matchers.define :have_page_body do |expected_page_body|
+  @page_body = ''
   match do |app|
     app_page = Machete::CF::AppPage.new(app)
-    app_page.body.include? expected_page_body
+    @page_body = app_page.body
+    @page_body.include? expected_page_body
+  end
+
+  failure_message do |app|
+    "'#{expected_page_body}' not found in: \n\n" +
+      @page_body
+  end
+
+  failure_message_when_negated do |host|
+    "'#{expected_page_body}' was found in: \n\n" +
+      @page_body
   end
 end
