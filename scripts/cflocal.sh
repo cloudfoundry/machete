4dbe3ff92a4dfc19d5cd26ec34946826a80915df
Rasheed Abdul-Aziz <pair+squeedee@pivotal.io>
2014-11-06 12:33:29 -0500

    Create concise script for environment configuration
    [#80936284]

--- scripts/cflocal.sh
+++
@@ -1,2 +0,0 @@
-cf api api.10.244.0.34.xip.io --skip-ssl-validation
-
--- scripts/configure_machines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --login
-
-./scripts/online_api
-./scripts/offline_api
-./scripts/setup_databases
-
-bundle
-VAGRANT_CWD=$HOME/workspace/bosh-lite-2nd-instance bundle exec ./scripts/enable_bosh_enterprise_firewall.rb
