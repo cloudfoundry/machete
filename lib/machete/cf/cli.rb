3f47ccabee99fc692cc64497523415ac697d58e8
JT Archie <jtarchie@gmail.com>
2015-03-20 11:46:41 -0400

    add better error messaging around missing app URL

--- lib/machete/cf/cli.rb
+++ lib/machete/cf/cli.rb
@@ -1,13 +1,22 @@
 module Machete
   module CF
     module CLI
-      CF_URL_REGEX = /^\s*urls:\s*(.*)/
+      CF_URL_REGEX  = /^\s*urls:\s*(.*)/
 
       class << self
         def url_for_app(app)
           cf_app_result = SystemHelper.run_cmd("cf app #{app.name}")
 
-          cf_app_result.match(CF_URL_REGEX)[1]
+          if match = cf_app_result.match(CF_URL_REGEX)
+            match[1]
+          else
+            raise <<-ERROR.chomp
+Failed finding app URL
+response:
+
+#{cf_app_result}
+ERROR
+          end
         end
       end
     end
--- spec/unit/cf/cli_spec.rb
+++ spec/unit/cf/cli_spec.rb
@@ -3,27 +3,35 @@ require 'spec_helper'
 module Machete
   module CF
     describe CLI do
-      describe 'an apps url' do
         let(:app) { double(
           :app,
           name: 'myApp'
         )}
-
-        before do
-          allow(SystemHelper).to receive(:run_cmd).
+      context 'an app has a URL' do
+        specify do
+          expect(SystemHelper).to receive(:run_cmd).
             with('cf app myApp').
             and_return(<<-RESPONSE)
               other stuff
               urls: the_apps_url
               other stuff
           RESPONSE
-        end
 
-        specify do
           expect(CLI.url_for_app(app)).to eql('the_apps_url')
         end
       end
 
+      context 'when the app does not have a URL' do
+        specify do
+          expect(SystemHelper).to receive(:run_cmd).
+            with('cf app myApp').
+            and_return('this is not the response you are looking for')
+
+          expect {
+            CLI.url_for_app(app)
+          }.to raise_error("Failed finding app URL\nresponse:\n\nthis is not the response you are looking for")
+        end
+      end
     end
   end
 end
