ec1aa8db00605d3c558395a5ff2f934568514caa
Anthony Emengo <aemengo@pivotal.io>
2015-06-04 10:35:58 -0400

    Add a spec for machete have_body matcher
    Signed-off-by: Colin Jackson <colin.s.jackson90@gmail.com>

---
+++ spec/unit/matchers/browser/have_body_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'machete/matchers'
+
+module Machete
+  describe '#have_body' do
+    let(:browser) { double(:browser, body: 'this is included!') }
+
+    context "when the response body does include the matched text" do
+      it "passes" do
+        expect(browser).to have_body "this is included!"
+      end
+    end
+
+    context "when the response body does not include the matched text" do
+      it "fails with a helpful error message" do
+        expect(browser).not_to have_body "this is not included :("
+      end
+    end
+  end
+end
