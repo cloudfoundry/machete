5f2645e8186efda7457395cfc2316224651f9337
Colin Jackson <colin.s.jackson90@gmail.com>
2015-06-29 13:58:35 -0400

    Switch language from online/offline to cached/uncached
    [#90408666]
    
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- lib/machete/buildpack_mode.rb
+++ lib/machete/buildpack_mode.rb
@@ -4,17 +4,17 @@ module Machete
       def buildpack_mode
         return @buildpack_mode if @buildpack_mode
 
-        @buildpack_mode = (ENV['BUILDPACK_MODE'] || :online).downcase.to_sym
+        @buildpack_mode = (ENV['BUILDPACK_MODE'] || :cached).downcase.to_sym
         Machete.logger.info("BUILDPACK_MODE not specified.\nDefaulting to '#{@buildpack_mode}'") unless ENV['BUILDPACK_MODE']
         @buildpack_mode
       end
 
-      def offline?
-        !online?
+      def cached?
+        !uncached?
       end
 
-      def online?
-        buildpack_mode == :online
+      def uncached?
+        buildpack_mode == :uncached
       end
     end
   end
