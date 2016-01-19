bf9c28a3beae40f38aa68cf36b3a4febcfc26639
James Wen <jrw2175@columbia.edu>
2016-01-19 11:29:11 -0500

    Set fixture app manifest.yml env variables in docker container for internet matcher
    [#111610094]
    
    Signed-off-by: David Jahn <david.a.jahn@gmail.com>

--- lib/machete/matchers/app_has_internet_traffic.rb
+++ lib/machete/matchers/app_has_internet_traffic.rb
@@ -1,11 +1,13 @@
 require 'rspec/matchers'
 require 'erb'
+require 'yaml'
 
 RSpec::Matchers.define :have_internet_traffic do
   dockerfile = <<-DOCKERFILE
 FROM cloudfoundry/cflinuxfs2
 
 ENV CF_STACK cflinuxfs2
+<%= docker_env_vars %>
 
 ADD <%= fixture_path %> /tmp/staged/
 ADD ./<%= cached_buildpack_path %> /tmp/
@@ -26,6 +28,21 @@ RUN (sudo tcpdump -n -i eth0 not udp port 53 and ip -c 1 -t | sed -e 's/^[^$]/in
       fixture_path = "./cf_spec/fixtures/#{app_name}"
 
       dockerfile_path = "Dockerfile.#{$$}.#{Time.now.to_i}"
+
+      manifest_search = Dir.glob("#{fixture_path}/**/manifest.yml")
+      manifest_location = ''
+      manifest_hash = {}
+      unless manifest_search.empty?
+        manifest_location = File.expand_path(manifest_search[0])
+        manifest_hash = YAML.load_file(manifest_location)
+      end
+      docker_env_vars = ''
+      if manifest_hash.has_key?('env')
+        manifest_hash['env'].each do |key, value|
+          docker_env_vars << "ENV #{key.to_s} #{value.to_s}\n"
+        end
+      end
+
       dockerfile_contents = ERB.new(dockerfile).result binding
       File.write(dockerfile_path, dockerfile_contents)
 
