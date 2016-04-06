a91f4808f820e6f4ad6eb17ad5f93a35132c3e3b
James Wen <jrw2175@columbia.edu>
2016-04-06 00:14:55 -0400

    Ensure that `has_internet_traffic` matcher integration tests actually run as part of rspec test suite
    - `has_internet_traffic` integration tests were previously not being run
      and were failing to incorrect test setup.

--- spec/integration/matchers/has_internet_traffic/cf.Gemfile
+++ spec/integration/matchers/has_internet_traffic/cf.Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem 'machete', path: '../'
+gem 'machete', path: '../../../..'
 gem 'buildpack-packager', git: 'https://github.com/cloudfoundry/buildpack-packager', tag: 'v2.2.6'
 gem 'rspec'
 gem 'rspec-instafail'
---
+++ spec/integration/matchers/has_internet_traffic/cf_spec/integration/has_internet_traffic_spec.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+require_relative '../spec_helper'
+require 'machete/matchers'
+
+module Machete
+  describe '#has_internet_traffic' do
+    let(:fixture_dir) { File.join(Dir.pwd, 'spec', 'integration', 'matchers',
+                                  'has_internet_traffic') }
+    subject(:app) do
+      Machete::App.new(app_name, { name: app_name, buildpack: 'null-test-buildpack'} )
+    end
+
+    before do
+      Dir.chdir(fixture_dir) do
+        system('BUNDLE_GEMFILE=cf.Gemfile bundle && BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --cached')
+      end
+    end
+
+    context 'an app that does not access the internet' do
+      let(:app_name) { 'app_that_does_not_access_the_internet' }
+
+      it 'logs no internet traffic' do
+        Dir.chdir(fixture_dir) do
+          expect(app).not_to have_internet_traffic
+        end
+      end
+    end
+
+    context 'an app that accesses the internet' do
+      let(:app_name)    { 'app_that_accesses_the_internet' }
+
+      it 'logs internet traffic' do
+        Dir.chdir(fixture_dir) do
+          expect(app).to have_internet_traffic
+        end
+      end
+    end
+  end
+end
