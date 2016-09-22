2a17405954e8f060fbf0b7b71759d9383e125550
Sam Smith <sesmith177@gmail.com>
2016-09-22 16:41:38 -0400

    Add extra `cf ssh` to have_file matcher
    - We suspect that on GCP there is a race condition where for a very
      small amount of time after the app has started, `cf ssh` will fail.
      This works around the problem by adding an extra `cf ssh` to eat the
      failure. This ensures the second `cf ssh` succeeds and hence the
      matcher can accurately detect the presence of the file.
    
    [#128991329]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- lib/machete/cf/app_file.rb
+++ lib/machete/cf/app_file.rb
@@ -12,6 +12,7 @@ module Machete
 
       def has_file?(filename)
         if app_has_diego_enabled?
+          SystemHelper.run_cmd("cf ssh #{app.name} -c 'ls /app'")
           SystemHelper.run_cmd("cf ssh #{app.name} -c 'ls #{filename}'")
         else
           SystemHelper.run_cmd("cf files #{app.name} #{filename}")
