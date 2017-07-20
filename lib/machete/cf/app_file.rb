34c2e131b9768d8a2d8f28b27f2866af24669372
Sam Coward <scoward@pivotal.io>
2017-07-20 10:53:35 -0400

    Assume that diego is always present now
    Since newer CF CLI no longer support the `has-diego-enabled` command
    
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

--- lib/machete/cf/app_file.rb
+++ lib/machete/cf/app_file.rb
@@ -11,19 +11,9 @@ module Machete
       end
 
       def has_file?(filename)
-        if app_has_diego_enabled?
-          SystemHelper.run_cmd("cf ssh #{app.name} -c 'ls /app'")
-          SystemHelper.run_cmd("cf ssh #{app.name} -c 'ls #{filename}'")
-        else
-          SystemHelper.run_cmd("cf files #{app.name} #{filename}")
-        end
+        SystemHelper.run_cmd("cf ssh #{app.name} -c 'ls /app'")
+        SystemHelper.run_cmd("cf ssh #{app.name} -c 'ls #{filename}'")
         SystemHelper.exit_status == 0
-      end
-
-      private
-
-      def app_has_diego_enabled?
-        SystemHelper.run_cmd("cf has-diego-enabled #{app.name}").chomp == 'true'
       end
     end
   end
--- spec/unit/cf/app_file_spec.rb
+++ spec/unit/cf/app_file_spec.rb
@@ -10,35 +10,7 @@ module Machete
       subject(:app_file) { AppFile.new(app) }
 
       describe '#has_file?' do
-        context 'with a standard CF deployment' do
-          before do
-            allow(SystemHelper).to receive(:run_cmd)
-              .with("cf has-diego-enabled #{app.name}").and_return('false')
-            allow(SystemHelper)
-              .to receive(:run_cmd)
-              .with('cf files app_name filename')
-          end
-
-          context 'when the file exists in the app' do
-            it 'returns true' do
-              expect(SystemHelper).to receive(:exit_status).and_return(0)
-              expect(app).to have_file('filename')
-            end
-          end
-
-          context 'when file does not exist in the app' do
-            it 'returns false' do
-              expect(SystemHelper).to receive(:exit_status).and_return(1)
-              expect(app).to_not have_file('filename')
-            end
-          end
-        end
-      end
-
-      context 'with a Diego CF deployment' do
         before do
-          allow(SystemHelper).to receive(:run_cmd)
-            .with("cf has-diego-enabled #{app.name}").and_return('true')
           allow(SystemHelper).to receive(:run_cmd)
             .with("cf ssh #{app.name} -c 'ls /app'")
           allow(SystemHelper).to receive(:run_cmd)
